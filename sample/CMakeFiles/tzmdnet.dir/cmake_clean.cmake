6da7447c85e1c50cbf070509b31b7a9f5cbd8b8a
Your Name <jakezhang1989@hotmail.com>
2017-12-10 13:25:02 +1100

    finished cmake draft and it is working

---
+++ sample/CMakeFiles/tzmdnet.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/tzmdnet.dir/tzmdnet.c.o"
+  "tzmdnet.pdb"
+  "tzmdnet"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/tzmdnet.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ src/CMakeFiles/zmdnet.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/zmdnet.dir/system/zmdnet-env.c.o"
+  "CMakeFiles/zmdnet.dir/adaption/zmdnet-socket.c.o"
+  "libzmdnet.pdb"
+  "libzmdnet.so.1.0.0"
+  "libzmdnet.so"
+  "libzmdnet.so.1"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/zmdnet.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
