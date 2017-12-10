273afef067772a591c9b92e42a29c27fd8b2fa03
Jake Zhang <jakezhang1989@hotmail.com>
2017-12-10 21:23:30 +1100

    generate 2017 vs project

---
+++ msvc/src/config.h
@@ -0,0 +1,57 @@
+
+#ifndef _GNU_SOURCE
+/* #undef _GNU_SOURCE */
+#endif
+
+#ifndef ZMDNET_DEBUG
+#define ZMDNET_DEBUG
+#endif
+
+#ifndef RUN_TIME_CHECKS
+#define RUN_TIME_CHECKS
+#endif
+
+#ifndef ZMDNET_SUPPORT_IPV4
+#define ZMDNET_SUPPORT_IPV4
+#endif
+
+#ifndef ZMDNET_SUPPORT_IPV6
+#define ZMDNET_SUPPORT_IPV6
+#endif
+
+#ifndef HAVE_SYS_QUEUE_H
+/* #undef HAVE_SYS_QUEUE_H */
+#endif
+
+#ifndef HAVE_LINUX_IF_ADDR_H
+/* #undef HAVE_LINUX_IF_ADDR_H */
+#endif
+
+#ifndef HAVE_LINUX_RTNETLINK_H
+/* #undef HAVE_LINUX_RTNETLINK_H */
+#endif
+
+#ifndef HAVE_NETINET_IP_ICMP_H
+/* #undef HAVE_NETINET_IP_ICMP_H */
+#endif
+
+#ifndef HAVE_STDATOMIC_H
+/* #undef HAVE_STDATOMIC_H */
+#endif
+
+#ifndef HAVE_SA_LEN
+/* #undef HAVE_SA_LEN */
+#endif
+
+#ifndef HAVE_SIN_LEN
+/* #undef HAVE_SIN_LEN */
+#endif
+
+#ifndef HAVE_SIN6_LEN
+/* #undef HAVE_SIN6_LEN */
+#endif
+
+#ifndef HAVE_SCONN_LEN
+/* #undef HAVE_SCONN_LEN */
+#endif
+
